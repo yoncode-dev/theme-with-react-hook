dc7bf63322bf89f6a934e3b58bf507f7bdc02263
Leandro Freitas <leandrodavimg@Leandros-MacBook-Pro.local>
2020-06-17 12:10:15 -0300

    update import css in public/index.html

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
 
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
   * {
     margin: 0;
     padding: 0;
